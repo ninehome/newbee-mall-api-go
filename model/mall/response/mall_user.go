5879475aff3139e0ddcdb9dc99379791a3f2895d
nine <nine.h@iscmango.com>
2022-11-19 09:41:30 +0800

    frist

---
+++ model/mall/response/mall_user.go
@@ -0,0 +1,9 @@
+package response
+
+type MallUserDetailResponse struct {
+	NickName      string `json:"nickName"`
+	LoginName     string `json:"loginName"`
+	IntroduceSign string `json:"introduceSign"`
+	UserMoney     int    `json:"userMoney"`
+	UserLevel     int    `json:"userLevel"`
+}
