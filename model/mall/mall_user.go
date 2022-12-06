e8af4a1ad6c919119c09ce1762810d06e48583a3
nine <nine.h@iscmango.com>
2022-12-06 10:25:10 +0800

    service

--- model/mall/mall_user.go
+++ model/mall/mall_user.go
@@ -9,7 +9,7 @@ type MallUser struct {
 	NickName      string          `json:"nickName" form:"nickName" gorm:"column:nick_name;comment:用户昵称;type:varchar(50);"`
 	LoginName     string          `json:"loginName" form:"loginName" gorm:"column:login_name;comment:登陆名称(默认为手机号);type:varchar(11);"`
 	UserMoney     int             `json:"userMoney" form:"userMoney" gorm:"column:user_money;comment:用户余额;type:tinyint"`
-	UserLevel     int             `json:"userLevel " form:"userLevel" gorm:"column:user_level;comment:用户等级;type:tinyint"`
+	UserLevel     int             `json:"userLevel" form:"userLevel" gorm:"column:user_level;comment:用户等级;type:tinyint"`
 	PasswordMd5   string          `json:"passwordMd5" form:"passwordMd5" gorm:"column:password_md5;comment:MD5加密后的密码;type:varchar(32);"`
 	IntroduceSign string          `json:"introduceSign" form:"introduceSign" gorm:"column:introduce_sign;comment:个性签名;type:varchar(100);"`
 	IsDeleted     int             `json:"isDeleted" form:"isDeleted" gorm:"column:is_deleted;comment:注销标识字段(0-正常 1-已注销);type:tinyint"`
