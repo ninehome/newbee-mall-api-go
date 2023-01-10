5adc5eae7c194cbf7fdb861007b2b7d89541241c
nine <nine.h@iscmango.com>
2023-01-10 08:50:55 +0800

    更新商品价格

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"main.go/core"
 	"main.go/global"
 	"main.go/initialize"
+	"os"
 )
 
 func main() {
@@ -14,5 +18,48 @@ func main() {
 	core.RunWindowsServer()           //设置路由,启动端口监听
 
 	//测试git更新
+
+}
+
+func readTXT() {
+	//打开文件
+	file, err := os.Open("./output.txt")
+	if err != nil {
+		fmt.Println("文件打开失败 = ", err)
+	}
+	//及时关闭 file 句柄，否则会有内存泄漏
+	defer file.Close()
+	//创建一个 *Reader ， 是带缓冲的
+	reader := bufio.NewReader(file)
+	for {
+		str, err := reader.ReadString('\n') //读到一个换行就结束
+		if err == io.EOF {                  //io.EOF 表示文件的末尾
+			break
+		}
+		fmt.Print(str)
+	}
+	fmt.Println("文件读取结束...")
+}
+
+func writerTxt() {
+	//创建一个新文件，写入内容
+	filePath := "./output.txt"
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Printf("打开文件错误= %v \n", err)
+		return
+	}
+	//及时关闭
+	defer file.Close()
+	//写入内容
+	str := "http://c.biancheng.net/golang/\n" // \n\r表示换行  txt文件要看到换行效果要用 \r\n
+	//写入时，使用带缓存的 *Writer
+	writer := bufio.NewWriter(file)
+	for i := 0; i < 3; i++ {
+		writer.WriteString(str)
+	}
+	//因为 writer 是带缓存的，因此在调用 WriterString 方法时，内容是先写入缓存的
+	//所以要调用 flush方法，将缓存的数据真正写入到文件中。
+	writer.Flush()
 
 }
