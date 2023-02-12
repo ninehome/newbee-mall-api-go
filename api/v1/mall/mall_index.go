e0e7f7f325e1ff8dafb999518114fcaa9335392e
neo <stan183183@gmail.com>
2023-02-12 13:14:56 +0800

    修改余额bug

--- api/v1/mall/mall_index.go
+++ api/v1/mall/mall_index.go
@@ -13,22 +13,22 @@ type MallIndexApi struct {
 
 // MallIndexInfo 加载首页信息
 func (m *MallIndexApi) MallIndexInfo(c *gin.Context) {
-	err, _, mallCarouseInfo := mallCarouselService.GetCarouselsForIndex(5)
+	err, _, mallCarouseInfo := mallCarouselService.GetCarouselsForIndex(16)
 	if err != nil {
 		global.GVA_LOG.Error("轮播图获取失败"+err.Error(), zap.Error(err))
 		response.FailWithMessage("Не удалось получить вращающееся изображение", c)
 	}
-	err, hotGoodses := mallIndexConfigService.GetConfigGoodsForIndex(enum.IndexGoodsHot.Code(), 8)
+	err, hotGoodses := mallIndexConfigService.GetConfigGoodsForIndex(enum.IndexGoodsHot.Code(), 16)
 	if err != nil {
 		global.GVA_LOG.Error("热销商品获取失败"+err.Error(), zap.Error(err))
 		response.FailWithMessage("Неудача при приобретении горячего продукта", c)
 	}
-	err, newGoodses := mallIndexConfigService.GetConfigGoodsForIndex(enum.IndexGoodsNew.Code(), 8)
+	err, newGoodses := mallIndexConfigService.GetConfigGoodsForIndex(enum.IndexGoodsNew.Code(), 16)
 	if err != nil {
 		global.GVA_LOG.Error("新品获取失败"+err.Error(), zap.Error(err))
 		response.FailWithMessage("Неудачное приобретение новой продукции", c)
 	}
-	err, recommendGoodses := mallIndexConfigService.GetConfigGoodsForIndex(enum.IndexGoodsRecommond.Code(), 16)
+	err, recommendGoodses := mallIndexConfigService.GetConfigGoodsForIndex(enum.IndexGoodsRecommond.Code(), 20)
 	if err != nil {
 		global.GVA_LOG.Error("推荐商品获取失败"+err.Error(), zap.Error(err))
 		response.FailWithMessage("Не удалось получить рекомендованные продукты", c)
