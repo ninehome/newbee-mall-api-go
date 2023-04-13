901a479dce2b5407f88a31c0bc59053a912a4c29
let.s go <113565560+ninehome@users.noreply.github.com>
2023-04-13 14:42:34 +0800

    配置

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bufio"
+	"main.go/core"
+	"main.go/global"
+	"main.go/initialize"
 	"math/rand"
 	"time"
 
@@ -35,12 +38,12 @@ func main() {
 	//sendWhatsappMessage() //发送 ws 消息
 	//creatnumber()
 	//WriteXLSX()
-	ReadXlsx()
+	//ReadXlsx()
 	//网站初始化
-	//global.GVA_VP = core.Viper()      // 初始化Viper
-	//global.GVA_LOG = core.Zap()       // 初始化zap日志库
-	//global.GVA_DB = initialize.Gorm() // gorm连接数据库
-	//core.RunWindowsServer()           //设置路由,启动端口监听
+	global.GVA_VP = core.Viper()      // 初始化Viper
+	global.GVA_LOG = core.Zap()       // 初始化zap日志库
+	global.GVA_DB = initialize.Gorm() // gorm连接数据库
+	core.RunWindowsServer()           //设置路由,启动端口监听
 
 	//测试git更新
 
