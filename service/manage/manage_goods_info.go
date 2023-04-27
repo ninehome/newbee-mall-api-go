0e0dec4e88f295421a890b069c805c2c6f2b2a17
let.s go <113565560+ninehome@users.noreply.github.com>
2023-04-27 17:41:52 +0800

    订单查询

--- service/manage/manage_goods_info.go
+++ service/manage/manage_goods_info.go
@@ -104,6 +104,26 @@ func (m *ManageGoodsInfoService) UpdateMallGoodsInfoV2(req manageReq.GoodsInfoUp
 	return err
 }
 
+// 商品倒计时
+func (m *ManageGoodsInfoService) UpdateGoodsInfoTime(req manageReq.GoodsInfoUpdateParamV2) (err error) {
+	goodsId, _ := strconv.Atoi(req.GoodsId)
+	//originalPrice, _ := strconv.Atoi(req.SellingPrice)
+	updateTime := time.Now().Unix() + 5*60
+
+	err = global.GVA_DB.Model(&manage.MallGoodsInfo{}).Where("goods_id = ?", goodsId).Update("count_time", updateTime).Error
+
+	return err
+}
+
+// 商品 停止倒计时
+func (m *ManageGoodsInfoService) StopCountDownTime(req manageReq.GoodsInfoUpdateParamV2) (err error) {
+	goodsId, _ := strconv.Atoi(req.GoodsId)
+
+	err = global.GVA_DB.Model(&manage.MallGoodsInfo{}).Where("goods_id = ?", goodsId).Update("count_time", 0).Error
+
+	return err
+}
+
 // GetMallGoodsInfo 根据id获取MallGoodsInfo记录
 func (m *ManageGoodsInfoService) GetMallGoodsInfo(id int) (err error, mallGoodsInfo manage.MallGoodsInfo) {
 	err = global.GVA_DB.Where("goods_id = ?", id).First(&mallGoodsInfo).Error
