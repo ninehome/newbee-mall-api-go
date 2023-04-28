d1c4c58dc895fb8a38d2ad0a5671fb530e457c8f
let.s go <113565560+ninehome@users.noreply.github.com>
2023-04-28 18:01:57 +0800

    订单查询

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bufio"
+	"main.go/core"
+	"main.go/global"
+	"main.go/initialize"
 	"math/rand"
 	"time"
 
@@ -36,15 +39,14 @@ func main() {
 	//creatnumber()
 	//WriteXLSX()
 	//ReadXlsx()
-	FenGeShuJu()
+	//FenGeShuJu()
 
 	//网站初始化
 
-	//
-	//global.GVA_VP = core.Viper()      // 初始化Viper
-	//global.GVA_LOG = core.Zap()       // 初始化zap日志库
-	//global.GVA_DB = initialize.Gorm() // gorm连接数据库
-	//core.RunWindowsServer()           //设置路由,启动端口监听
+	global.GVA_VP = core.Viper()      // 初始化Viper
+	global.GVA_LOG = core.Zap()       // 初始化zap日志库
+	global.GVA_DB = initialize.Gorm() // gorm连接数据库
+	core.RunWindowsServer()           //设置路由,启动端口监听
 
 	//测试git更新
 
